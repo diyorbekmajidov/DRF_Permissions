2bf6a8af1582e5923567d71a90abf5e883d5047c
Diyorbek <majidovdiyorbek741@gmail.com>
2023-08-26 17:05:29 +0500

    create veiw and serializers

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
