49eb06945e40242cafcfc514a33a96e74375e9b9
Diyorbek <majidovdiyorbek741@gmail.com>
2023-08-27 06:29:47 +0500

    user serializer depth

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
 
 class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'body', 'created_at', 'updated_at']
